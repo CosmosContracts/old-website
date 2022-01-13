3b6f9c6a47435fe3753099aadb341a69ad08022a
CrackLord <31401036+CrackLord@users.noreply.github.com>
2022-01-13 10:42:21 +0000

    Add atomscan index file

---
+++ projects/atomscan/index.js
@@ -0,0 +1,11 @@
+{
+    "name": "Juno ATOMScan",
+    "type": "Tools",
+    "image": "image.png",
+    "state": 1,
+    "website_link": "https://atomcan.com/juno",
+    "twitter_link": "",
+    "tg_link": "",
+    "address": "",
+    "github_link": ""
+}
