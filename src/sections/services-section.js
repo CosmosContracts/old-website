27755bf227a5aae0d665265090b40c7cfc677b90
SeraGabor <sragabor@gmail.com>
2022-03-08 11:03:30 +0100

    remove contributors section

--- src/sections/services-section.js
+++ src/sections/services-section.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {StaticImage} from "gatsby-plugin-image";
-import Contributors from "../components/contributors";
 import PiechartSection from "./piechart-section";
 
 import { Parallax } from 'react-scroll-parallax';
@@ -171,33 +170,6 @@ export function ServicesSection() {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-
-                    <div className={'row contributors align-items-center'}>
-                        <div className={'col-12'}>
-                            <div className={'row justify-content-between align-items-center'}>
-                                <div className={'col-auto'}>
-                                    <h3
-                                        data-sal="fade"
-                                        data-sal-delay="200"
-                                        data-sal-duration="1000">
-                                        Core <br/>
-                                        Contributors
-                                    </h3>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={'col-12 pt-4'}>
-                            <Contributors/>
-                        </div>
-                        <div className={'col-12 pt-4'}
-                             data-sal="fade"
-                             data-sal-delay="400"
-                             data-sal-duration="1000">
-                            <a href={menu['view-all-contributors']} target={'_blank'} rel={'noreferrer'}>
-                                <div className={'link'}><i className={'icon-arrow-right'} aria-label={'View all'}></i>View all</div>
-                            </a>
                         </div>
                     </div>
 
