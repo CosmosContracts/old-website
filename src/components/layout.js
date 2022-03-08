eaeaa4a6fed9aa412f91e920ae5b7ae03c32b212
SeraGabor <sragabor@gmail.com>
2022-03-08 16:20:12 +0100

    fix helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,10 @@ import Header from "./header";
 import {Helmet} from "react-helmet";
 import { ParallaxProvider } from 'react-scroll-parallax';
 
+import ogimage from '../images/ogimage.png';
+import favicon from '../images/favicons/apple-icon-57x57.png';
+import favicon72 from '../images/favicons/apple-icon-72x72.png';
+
 export default function Layout({ children }) {
 
     return (
@@ -17,16 +21,35 @@ export default function Layout({ children }) {
 
                 <meta charSet="utf-8" />
                 <title>Juno - Smart Contracts Zone</title>
-                <meta content="Juno is an interoperable smart contract network and a zone part of the Cosmos Network. Highly scalable, robust, secure and easy to deploy!" name="description"/>
+                <meta name={'title'} content={'Juno - Smart Contracts Zone'} />
+                <meta name={'description'} content={'Juno is an interoperable smart contract network and a zone part of the Cosmos Network. Highly scalable, robust, secure and easy to deploy!'} />
+
                 <meta content="Smart Contracts Zone" property="og:site_name"/>
                 <meta content="https://junonetwork.io/" property="og:url"/>
-                <meta content="/src/images/ogimage.png" property="og:image"/>
-
-                <link rel="apple-touch-icon" href="/src/images/favicons/apple-icon-57x57.png" />
-                <link rel="apple-touch-icon" sizes="72x72" href="/src/images/favicons/apple-icon-72x72.png" />
+                <meta property="og:title" content="Juno - Smart Contracts Zone" />
+                <meta name={'og:description'} content={'Juno is an interoperable smart contract network and a zone part of the Cosmos Network. Highly scalable, robust, secure and easy to deploy!'} />
+                <meta content={ogimage} property="og:image"/>
 
                 <link rel="canonical" href="https://junonetwork.io/" />
+
+                <link rel="apple-touch-icon" href={favicon} />
+                <link rel="apple-touch-icon" sizes="72x72" href={favicon72} />
+
                 <meta name="theme-color" content="#312526"/>
+
+                <meta property="article:publisher" content="https://junonetwork.io/" />
+                <meta property="og:site_name" content="Juno - Smart Contracts Zone" />
+                <meta property="og:type" content="website" />
+                <meta name="author" content="Juno - Smart Contracts Zone" />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:text:title" content="Juno - Smart Contracts Zone" />
+                <meta property="og:url" content="https://junonetwork.io/" />
+
+                <meta property="og:image" content={ogimage} key="ogimage" />
+                <meta property="og:image:height" content={944} />
+                <meta property="og:image:width" content={1905} />
+                <meta name="twitter:image" content={ogimage} key="twimage" />
+                <meta name="twitter:card" content="summary_large_image"/>
 
             </Helmet>
             <Header/>
